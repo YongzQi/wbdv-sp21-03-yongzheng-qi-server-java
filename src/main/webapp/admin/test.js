35d31c605f1ae0e5c6be571f34dc4135fec14506
yzqi <yongqi014@gmail.com>
2021-02-08 16:54:08 -0800

    added service

---
+++ src/main/webapp/admin/test.js
@@ -0,0 +1,86 @@
+jQuery(main)
+
+
+
+function main(){
+
+const STUDENT = "STUDENT";
+
+    let alice = {
+        username: "username",
+        password: "password",
+        firstName: "firstName",
+        lastName: "lastName",
+        role: STUDENT,
+
+    }
+
+    let $users = [
+        alice,
+    ]
+
+
+    let $h2 = $("#mainTitle")
+
+    $h2.html("Changed")
+
+
+
+    let $userList = $("#userList")
+    $userList.append("<tr> <td>alice</td> <td> FO </td> <td>BS</td> </tr>")
+
+    $userList.append($user)
+
+    let $user = $("<tr> <td>test</td> <td>&nbsp</td> </tr>")
+    console.log(alice)
+
+    let $userLi = $(`
+    <tr> <td>`+ $users[0].username + `</td> <td> this is test 2 </td> <td> im bored </td> </tr>
+    `)
+
+    $userList.append($user)
+
+    $userList.append("<tr class='wbdv-template wbdv-user wbdv-hidden'>" +
+        "<td class='wbdv-username'>"+$users[0].username + "</td> " +
+        "<td> FO </td> " +
+        "<td class='wbdv-first-name'>najfdj</td>" +
+        "<td class='wbdv-last-name'>last name</td>"+
+        "<td class='wbdv-role'>Student</td>" +
+        "<td class='wbdv-actions'> " +
+        "<span class='float-center'>" +
+        "<button class='wbdv-remove' style='border: none'><i id='wbdv-remove' class='fa-2x fa fa-times wbdv-remove'></i></button>" +
+        " <i id='wbdv-edit' class='fa-2x fa fa-pencil wbdv-edit'></i>" +
+        "</span>" +
+        "</td>" +
+        " </tr>")
+
+
+}
+
+
+let username = "alice"
+let password = "12324355"
+let firstName = "Alice"
+let lastName = "lastName"
+
+const FACULTY = "FACULTY"
+const STUDENT = "STUDENT"
+const ADMIN = "ADMIN"
+
+let role = STUDENT
+
+
+let alice = {
+    username: username,
+    password: password,
+    firstName: "firstName",
+    lastName: "lastName",
+    role: STUDENT,
+
+}
+
+let $users = [
+    alice,
+    {username: "xyz", password: "asdmkfs", firstName: "last", lastName: "lastname", role: FACULTY},
+    {username: "aasnf", password: "asfkf", firstName: "ksksja", lastName: "asfddf", role: FACULTY}
+]
